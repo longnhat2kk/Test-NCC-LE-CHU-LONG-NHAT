46a927edf45caeb07b54fd080e5526a8a3314681
longnhat2kk <longnhat212000@gmail.com>
2023-02-08 15:26:34 +0700

    up final

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const addBtn = document.getElementById("menu-mobile");
 const showMenu = document.getElementById("show-menu");
 const removeBtn = document.getElementById("close-menu");
-// const removeBtn = document.getElementById("close-icon");
+const closeicon = document.getElementById("close-icon");
+const showBg = document.getElementById("showBg");
 
 showMenu.addEventListener("click", () => {
   removeBtn.classList.add("show");
 });
 
+closeicon.addEventListener("click", () => {
+  removeBtn.classList.remove("show");
+});
+
 showMenu.addEventListener("click", () => {
-  //   removeBtn.classList.remove("show");
+  showBg.classList.add("show");
+});
+
+closeicon.addEventListener("click", () => {
+  showBg.classList.remove("show");
+});
+showBg.addEventListener("click", () => {
+  removeBtn.classList.remove("show");
+  showBg.classList.remove("show");
 });
